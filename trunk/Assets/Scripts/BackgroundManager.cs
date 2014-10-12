5249d57631540a340c6b504d9fc6f49e31d03349
delandojuninho@gmail.com <delandojuninho@gmail.com@60b927f7-ce23-c5b6-9a94-491509d3e0e3>
2014-10-12 03:30:39 +0000

    commit com imagens, e eventos funcionando...
    precisar colocar os audios definitivos.

---
+++ trunk/Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour 
+{
+	public float speed;
+	private float _offSet;
+	
+	// Use this for initialization
+	void Start()
+	{
+		_offSet = 0;
+		renderer.material.mainTextureOffset = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		_offSet += speed;
+		if (_offSet > 1f)   
+			_offSet -= 1f;
+		
+		renderer.material.mainTextureOffset = Vector2.right * _offSet;
+	}
+}
---
+++ trunk/Assets/Scripts/ObjectBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectBehavior : MonoBehaviour {
+	public float speed;
+	public GameObject meteoro;
+	private bool _isGameOver;
+
+
+	// Use this for initialization
+	void Start () {
+		_isGameOver = false;
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		if (_isGameOver) return;
+		transform.position += new Vector3 (speed, 0, 0) * Time.deltaTime;
+		if (transform.position.x < -10f) {
+			meteoro.SetActive(false);
+
+	
+	}
+	}
+
+	void GameEnd()
+	{
+		_isGameOver = true;
+	}
+}
