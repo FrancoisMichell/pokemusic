0e0dc87e98c27e557d566a877740cf7296be05ac
delandojuninho@gmail.com <delandojuninho@gmail.com@60b927f7-ce23-c5b6-9a94-491509d3e0e3>
2014-10-12 03:30:39 +0000

    commit com imagens, e eventos funcionando...
    precisar colocar os audios definitivos.

---
+++ Assets/Scripts/DigletScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DigletScript : MonoBehaviour {
+
+	private Transform digletDo, digletRe, digletMi, digletFa, digletSol, digletLa, digletSi;
+	private List<Transform> listadiglets = new List<Transform>();
+
+
+	// Use this for initialization
+	void Start () {
+		digletDo = transform.FindChild ("diglett-do");
+		digletRe = transform.FindChild ("diglett-re");
+		digletMi = transform.FindChild ("diglett-mi");
+		digletFa = transform.FindChild ("diglett-fa");
+		digletSol = transform.FindChild ("diglett-sol");
+		digletLa = transform.FindChild ("diglett-la");
+		digletSi = transform.FindChild ("diglett-si");
+		listadiglets.Add (digletDo);
+		listadiglets.Add (digletRe);
+		listadiglets.Add (digletMi);
+		listadiglets.Add (digletFa);
+		listadiglets.Add (digletSol);
+		listadiglets.Add (digletLa);
+		listadiglets.Add (digletSi);
+
+
+		Upall ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//Debug.Log("here");
+
+
+		if (Input.GetMouseButtonDown (0)) {
+			Vector2 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			Collider2D[] col = Physics2D.OverlapPointAll (pos);
+
+			if(col.Length > 0)
+				foreach (Collider2D c in col)
+					if (c.CompareTag("diglet")){
+						Hitdiglet(c.transform);
+						print(c.transform);
+					}
+			
+		}
+
+
+
+	
+	}
+
+	void Upall() {
+		foreach (Transform i in listadiglets){
+			print (i);
+			Updiglet (i);
+		}
+
+	}
+
+	void Updiglet(Transform a){
+		a.rigidbody2D.gravityScale = -5;
+
+	}
+
+	void Hitdiglet(Transform b){
+		b.rigidbody2D.velocity = Vector3.down*10;
+		b.audio.Play ();
+	}
+}
---
+++ Assets/Scripts/ObjectController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectController : MonoBehaviour {
+	public float maxHeight;
+	public float minHeight;
+
+	public float rateSpawn;
+	private float currentRateSpawn;
+
+	public int maxMeteoro;
+	
+	public GameObject prefab;
+
+	private bool _isGameOver;
+
+	public List<GameObject> meteoro;
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i < maxMeteoro; i++) {
+			GameObject tempMeteoro = Instantiate(prefab) as GameObject;
+			meteoro.Add(tempMeteoro);
+			tempMeteoro.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		currentRateSpawn += Time.deltaTime;
+		if (currentRateSpawn > rateSpawn) {
+						if (_isGameOver) return;
+						currentRateSpawn = 0;
+						Spawn();
+				}
+	
+	}
+	private void Spawn() {
+
+		float randPosition = Random.Range (minHeight, maxHeight);
+		GameObject tempMeteoro = null;
+		for (int i=0; i < maxMeteoro; i++) {
+			if(meteoro[i].activeSelf == false) {
+				tempMeteoro = meteoro[i];
+				break;
+			}
+		}
+			if (tempMeteoro != null) {
+			tempMeteoro.transform.position = new Vector3(transform.position.x, randPosition, transform.position.y);
+				tempMeteoro.SetActive(true);
+
+
+
+			}
+	}
+	void GameEnd()
+	{
+		_isGameOver = true;
+	}
+}
