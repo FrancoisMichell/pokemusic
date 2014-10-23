c80400953e610be04b3f18c7b3ca8de051b712e1
francoismichell95@gmail.com <francoismichell95@gmail.com@60b927f7-ce23-c5b6-9a94-491509d3e0e3>
2014-10-23 20:12:46 +0000

    Criação da lista de lançamento das nuvens no modo Genious Atribuição da nuvem correspondente ao elemento da lista Lançamento da nuvem correspondente ao elemento da lista

--- trunk/Assets/Scripts/ObjectController.cs
+++ trunk/Assets/Scripts/ObjectController.cs
@@ -10,25 +10,62 @@ public class ObjectController : MonoBehaviour {
 	private float currentRateSpawn;
 
 	public int maxNuvem;
-	
-	public GameObject prefab;
+
+	public GameObject nuvemDo, nuvemRe, nuvemMi, nuvemFa, nuvemSol, nuvemLa, nuvemSi;
 
 	private bool _isGameOver;
 
 	public List<GameObject> nuvem;
 
+	private List<int> ordem = new List<int>();
+	private int numero;
 
+	private GameObject nuvemSeguinte;
+
+	
 	// Use this for initialization
 	void Start () {
-		for (int i=0; i < maxNuvem; i++) {
-			GameObject tempNuvem = Instantiate(prefab) as GameObject;
-			nuvem.Add(tempNuvem);
-			tempNuvem.SetActive(false);
+
+				for (int i=0; i<maxNuvem; i++) {
+						numero = Random.Range (0, 7);
+						ordem.Add (numero);
+				}
+				for (int i=0; i<maxNuvem; i++) {
+						switch (ordem [i]) {
+						case 0:
+								nuvemSeguinte = nuvemDo;
+								break;
+						case 1:
+								nuvemSeguinte = nuvemRe;
+								break;
+						case 2:
+								nuvemSeguinte = nuvemMi;
+								break;
+						case 3:
+								nuvemSeguinte = nuvemFa;
+								break;
+						case 4:
+								nuvemSeguinte = nuvemSol;
+								break;
+						case 5:
+								nuvemSeguinte = nuvemLa;
+								break;
+						case 6:
+								nuvemSeguinte = nuvemSi;
+								break;
+						default:
+								break;
+						}
+						GameObject nuvemAtual = Instantiate (nuvemSeguinte) as GameObject;
+						nuvem.Add (nuvemAtual);
+						nuvemAtual.SetActive (false);
+				}
 		}
-	}
-	
 	// Update is called once per frame
 	void Update () {
+
+
+
 		currentRateSpawn += Time.deltaTime;
 		if (currentRateSpawn > rateSpawn) {
 						if (_isGameOver) return;
@@ -40,21 +77,23 @@ public class ObjectController : MonoBehaviour {
 	private void Spawn() {
 
 		float randPosition = Random.Range (AlturaMax, AlturaMin);
-		GameObject tempNuvem = null;
+		GameObject nuvemAtual = null;
 		for (int i=0; i < maxNuvem; i++) {
 			if(nuvem[i].activeSelf == false) {
-				tempNuvem = nuvem[i];
+				nuvemAtual = nuvem[i];
 				break;
 			}
 		}
-			if (tempNuvem != null) {
-			tempNuvem.transform.position = new Vector3(transform.position.x, randPosition, transform.position.y);
-				tempNuvem.SetActive(true);
+			if (nuvemAtual != null) {
+			nuvemAtual.transform.position = new Vector3(transform.position.x, randPosition, transform.position.y);
+				nuvemAtual.SetActive(true);
 
 
 
 			}
 	}
+
+
 	void GameEnd()
 	{
 		_isGameOver = true;
