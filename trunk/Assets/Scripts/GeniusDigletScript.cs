c71d9f1ab9c5095851ca8885ba6c65de15fbc144
francoismichell95@gmail.com <francoismichell95@gmail.com@60b927f7-ce23-c5b6-9a94-491509d3e0e3>
2014-11-25 21:50:25 +0000

    - Implementação da mudança dos BackGrounds INGAME - Implementação do morcego no background de Halloween - Implementação da quantidade de moedas ganhas na tela da loja - Implementação do design da loja

--- trunk/Assets/Scripts/GeniusDigletScript.cs
+++ trunk/Assets/Scripts/GeniusDigletScript.cs
@@ -4,80 +4,84 @@ using System.Collections.Generic;
 
 public class GeniusDigletScript : MonoBehaviour {
 
-	private Transform digletDo, digletRe, digletMi, digletFa, digletSol, digletLa, digletSi;
-	private List<Transform> listadiglets = new List<Transform>();
-	//sequencia de strings enviadas pelo objectController 
-	private List<string> sequencia = new List<string> ();
-	private bool touch;
-	//o indice da lista de sequencias que esta sendo usado atualmente
-	private int posicaoSequencia = 0;
-	private int tamanhoSequencia = 1;
+    private Transform digletDo, digletRe, digletMi, digletFa, digletSol, digletLa, digletSi;
+    private List<Transform> listadiglets = new List<Transform>();
+    //sequencia de strings enviadas pelo objectController 
+    private List<string> sequencia = new List<string>();
+    private bool touch;
+    //o indice da lista de sequencias que esta sendo usado atualmente
+    private int posicaoSequencia = 0;
+    private int tamanhoSequencia = 1;
 
-	private GameObject objectController;
-	public GameObject barraStatus1;
-	public GameObject barraStatus2;
+    private GameObject objectController;
+    public GameObject barraStatus1;
+    public GameObject barraStatus2;
 
-	public TextMesh Score;
+    public TextMesh Score;
     public TextMesh Score2;
-	
+
     public TextMesh HiScore;
     public TextMesh Moedas;
 
-	private int _score;
-	private int _hiscore;
+    private int _score;
+    private int _hiscore;
+    public int _moedas;
 
+    public int moedasTotais;
+    private int contaPontos;
 
-	private int toque;
+    private int toque;
     private bool pausado;
 
-	private GameObject geniusCamera;
-	private GameObject maquinaGenius;
-	
-
-	// Use this for initialization
-	void Start () {		
-		digletDo = transform.FindChild ("diglettDo");
-		digletRe = transform.FindChild ("diglettRe");
-		digletMi = transform.FindChild ("diglettMi");
-		digletFa = transform.FindChild ("diglettFa");
-		digletSol = transform.FindChild ("diglettSol");
-		digletLa = transform.FindChild ("diglettLa");
-		digletSi = transform.FindChild ("diglettSi");
-		
-		objectController = GameObject.FindGameObjectWithTag ("Launcher");
-		geniusCamera = GameObject.FindGameObjectWithTag ("MainCamera");
-		//A maquina do modo Genius esta setada com a tag "BarraSom"
-		maquinaGenius = GameObject.FindGameObjectWithTag ("BarraSom");
-		
-		listadiglets.Add (digletDo);
-		listadiglets.Add (digletRe);
-		listadiglets.Add (digletMi);
-		listadiglets.Add (digletFa);
-		listadiglets.Add (digletSol);
-		listadiglets.Add (digletLa);
-		listadiglets.Add (digletSi);
-
-		_hiscore = PlayerPrefs.GetInt ("hiscore", 0);
-		HiScore.text = "" + _hiscore;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// codigo usado para visualização na unity
-
-		//caso o indice da sequencia que esta sendo considerada neste momento for igual ao tamanho da lista de sequencias
-		//entao ele vai mostrar o "bom trabalho" e pedir uma nova sequencia
-		if (posicaoSequencia == sequencia.Count) {
-			this.barraStatus1.transform.position = new Vector3(barraStatus2.transform.position.x, barraStatus2.transform.position.y, -5);
-			posicaoSequencia = 0;
+    private GameObject geniusCamera;
+    private GameObject maquinaGenius;
 
 
-			//objectController.SendMessage("startPlay");
-			//esse 3 significa que ele ira esperar 3 segundos para ser executado
-			Invoke("pedirSequencia",1f);
+
+    // Use this for initialization
+    void Start() {
+        digletDo = transform.FindChild("diglettDo");
+        digletRe = transform.FindChild("diglettRe");
+        digletMi = transform.FindChild("diglettMi");
+        digletFa = transform.FindChild("diglettFa");
+        digletSol = transform.FindChild("diglettSol");
+        digletLa = transform.FindChild("diglettLa");
+        digletSi = transform.FindChild("diglettSi");
+
+        objectController = GameObject.FindGameObjectWithTag("Launcher");
+        geniusCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        //A maquina do modo Genius esta setada com a tag "BarraSom"
+        maquinaGenius = GameObject.FindGameObjectWithTag("BarraSom");
+
+        listadiglets.Add(digletDo);
+        listadiglets.Add(digletRe);
+        listadiglets.Add(digletMi);
+        listadiglets.Add(digletFa);
+        listadiglets.Add(digletSol);
+        listadiglets.Add(digletLa);
+        listadiglets.Add(digletSi);
+
+        _hiscore = PlayerPrefs.GetInt("hiscore", 0);
+        HiScore.text = "" + _hiscore;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // codigo usado para visualização na unity
+
+        //caso o indice da sequencia que esta sendo considerada neste momento for igual ao tamanho da lista de sequencias
+        //entao ele vai mostrar o "bom trabalho" e pedir uma nova sequencia
+        if (posicaoSequencia == sequencia.Count) {
+            this.barraStatus1.transform.position = new Vector3(barraStatus2.transform.position.x, barraStatus2.transform.position.y, -5);
+            posicaoSequencia = 0;
 
 
-		}
+            //objectController.SendMessage("startPlay");
+            //esse 3 significa que ele ira esperar 3 segundos para ser executado
+            Invoke("pedirSequencia", 1f);
+
+
+        }
         if (pausado == false) {
 
             if (Input.GetMouseButtonDown(0) && touch == true && toque < sequencia.Count) {
@@ -97,7 +101,7 @@ public class GeniusDigletScript : MonoBehaviour {
                         } else if (c.CompareTag("BtMenu")) {
                             Application.LoadLevel("Menu");
                         } else if (c.CompareTag("colPausa")) {
-                            break ;
+                            break;
                         }
 
 
@@ -115,95 +119,109 @@ public class GeniusDigletScript : MonoBehaviour {
                     }
                 }
             }
-        } 
+        }
 
-	}
+    }
+    //Codigo para multitoque. Esta comentado, pois so funciona no smartphone 
 
-		//Codigo para multitoque. Esta comentado, pois so funciona no smartphone 
-		
-		//		Touch myTouch = Input.GetTouch(0);
-		//		Vector2 pos = Camera.main.ScreenToWorldPoint(myTouch.position);
-		//		Collider2D[] myTouches = Physics2D.OverlapPointAll(pos);
-		//		if (myTouches.Length > 0 && touch == true){
-		//			foreach(Collider2D c in myTouches){
-		//					Hitdiglet(c.transform);
-		//
-		//			}
-		//		}
+    //		Touch myTouch = Input.GetTouch(0);
+    //		Vector2 pos = Camera.main.ScreenToWorldPoint(myTouch.position);
+    //		Collider2D[] myTouches = Physics2D.OverlapPointAll(pos);
+    //		if (myTouches.Length > 0 && touch == true){
+    //			foreach(Collider2D c in myTouches){
+    //					Hitdiglet(c.transform);
+    //
+    //			}
+    //		}
 
     public void Upall() {
-		foreach (Transform i in listadiglets){
-			Updiglet (i);
-			touch = true;
-		}
-	}
-	
-	void Updiglet(Transform a){
-		a.rigidbody2D.gravityScale = -5;
-	}
-	
-	void Hitdiglet(Transform b){
-		b.audio.Play ();
-		b.rigidbody2D.velocity = Vector3.down * 10;
-	}
-	
-	public void downDigglets(){
-		foreach (Transform i in listadiglets) {
-			i.rigidbody2D.gravityScale = 5;
-			touch = false;
-		}
-	}
-	
-	public void receberSequencia(List<string> lista){
-		this.sequencia = lista; 
-	}
-	
-	public void pedirSequencia(){
-		tamanhoSequencia += 1;
-		barraStatus1.transform.position = new Vector3(barraStatus2.transform.position.x, barraStatus2.transform.position.y, 5);
-		downDigglets();
-		print (tamanhoSequencia);
-		objectController.SendMessage("gerarSequencia", tamanhoSequencia);
-		toque = 0;
-	}
-	
-	public void pedirSequenciaInicio(){
-		barraStatus2.transform.position = new Vector3(barraStatus1.transform.position.x, barraStatus1.transform.position.y, 5);
-		downDigglets();
-		tamanhoSequencia = 1;
-		objectController.SendMessage("gerarSequencia", tamanhoSequencia);
-	}
+        foreach (Transform i in listadiglets) {
+            Updiglet(i);
+            touch = true;
+        }
+    }
 
-	void PlayerScored(){
-		_score++;
-		Score.text = "" + _score;
+    void Updiglet(Transform a) {
+        a.rigidbody2D.gravityScale = -5;
+    }
+
+    void Hitdiglet(Transform b) {
+        b.audio.Play();
+        b.rigidbody2D.velocity = Vector3.down * 10;
+    }
+
+    public void downDigglets() {
+        foreach (Transform i in listadiglets) {
+            i.rigidbody2D.gravityScale = 5;
+            touch = false;
+        }
+    }
+
+    public void receberSequencia(List<string> lista) {
+        this.sequencia = lista;
+    }
+
+    public void pedirSequencia() {
+        tamanhoSequencia += 1;
+        barraStatus1.transform.position = new Vector3(barraStatus2.transform.position.x, barraStatus2.transform.position.y, 5);
+        downDigglets();
+        print(tamanhoSequencia);
+        objectController.SendMessage("gerarSequencia", tamanhoSequencia);
+        toque = 0;
+    }
+
+    public void pedirSequenciaInicio() {
+        barraStatus2.transform.position = new Vector3(barraStatus1.transform.position.x, barraStatus1.transform.position.y, 5);
+        downDigglets();
+        tamanhoSequencia = 1;
+        objectController.SendMessage("gerarSequencia", tamanhoSequencia);
+    }
+
+    void PlayerScored() {
+        _score++;
+        Score.text = "" + _score;
         Score2.text = "" + _score;
 
+        contaPontos++;
+        if (contaPontos > 9) {
+            _moedas += 1;
+            contaPontos = 0;
+            addMoedas();
+        }
 
-        Moedas.text = "" + _score/10;
+        Moedas.text = "" + _moedas;
 
-		if (_score > _hiscore) {
-			_hiscore = _score;
-			HiScore.text = "" + _hiscore;
-			PlayerPrefs.SetInt ("hiscore", _hiscore);
-		}
-	}
-	void moverCamera() 
-	{
-		geniusCamera.SendMessage ("paraScore");
-	}
+        if (_score > _hiscore) {
+            _hiscore = _score;
+            HiScore.text = "" + _hiscore;
+            PlayerPrefs.SetInt("hiscore", _hiscore);
+        }
 
-    void Pausar(){
+    }
+
+    private void addMoedas() {
+
+        PlayerPrefs.SetInt("moedas", _moedas + moedasTotais);
+    }
+
+    void moverCamera() {
+        geniusCamera.SendMessage("paraScore");
+    }
+
+    void Pausar() {
         pausado = true;
         Time.timeScale = 0f;
     }
-    void Continuar(){
+
+    void Continuar() {
         pausado = false;
         toque -= 1;
         Time.timeScale = 1f;
     }
-	void desativarMaquina()
-	{
-		maquinaGenius.SetActive(false);
-	}
+
+    void desativarMaquina() {
+        maquinaGenius.SetActive(false);
+    }
+
 }
+    
